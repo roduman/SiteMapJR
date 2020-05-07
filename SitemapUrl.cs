3b353d1b2e41a24f2f952b579f15123deef70c8f
roduman <ivanroduman@gmail.com>
2020-05-07 09:19:08 +0400

    Upload Project

---
+++ SitemapUrl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SiteMapJR
+{
+    public enum ChangeFrequency
+    {
+        Always,
+        Hourly,
+        Daily,
+        Weekly,
+        Monthly,
+        Yearly,
+        Never
+    }
+
+    public class SitemapUrl
+    {
+        public string Url { get; set; }
+        public DateTime? Modified { get; set; }
+        public ChangeFrequency? ChangeFrequency { get; set; }
+        public double? Priority { get; set; }
+    }
+}
